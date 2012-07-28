5d53896bc0a9d0e0c2e56318312924db89f4c26e
Echo-Flow Info <jbeard4@ps16821.dreamhostps.com>
2012-07-28 11:58:09 -0700

    Added additional checks for robustness. Works well.

--- serve.js
+++ serve.js
@@ -1,11 +1,62 @@
 //initially, pull from archive.org's main page, eval result. use that to pull down the track, and play it
-var http = require('http');
+var http = require('http'),
+    archive = require('./archive-lib'),
+    async = require('async'),
+    urlModule = require('url');
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(
-        '<?xml version="1.0" encoding="UTF-8"?>' +
-        '<Response>' + 
-        '    <Say voice="woman" language="fr">Chapeau!</Say>' + 
-        '</Response>');
+
+    function handleError(err){
+        res.writeHead(500, {'Content-Type': 'text/plain'});
+        res.end(err.message);
+    }
+
+    archive.getPicksOfTheDay(function(err,picks){
+        if(err) return handleError(err);
+
+        archive.getDetail(picks.etree.identifier,function(err,detail){
+            if(err) return handleError(err);
+
+            var urls = archive.getUrlsFromDetail(detail).
+                            filter(function(url){return url.match(/.mp3$/);}).
+                            filter(function(url){return !url.match(/_vbr.mp3$/);});  //filter out vbr-encoded mp3s
+
+            //get the urls to make sure they actually work
+            async.filter(urls,function(url,cb){
+                var oUrl = urlModule.parse(url); 
+
+                var opt = {
+                    host : oUrl.host,
+                    path : oUrl.path,
+                    port : 80,
+                    protocol : oUrl.protocol,
+                    method : 'HEAD'
+                };
+                
+                console.log("requesting ",url,opt);
+                var req = http.request(opt,function(res){
+                    console.log("received response to url",url,res.statusCode);
+                    cb(res.statusCode === 200);
+                });
+                req.on("error",function(e){
+                    console.log("Received http error when requesting url.",e);
+                    cb(false);
+                });
+                req.end();
+            },function(goodUrls){
+                if(err) return handleError(err);
+
+                res.writeHead(200, {'Content-Type': 'application/xml'});
+                res.end(
+                    '<Response>' +
+                        '<Say>Playing the archive dot org Live music picks</Say>' +
+                        '<Say>' +  picks.etree.title + '</Say>' +
+                        '<Say>Please wait while the songs are loaded</Say>' +
+                        goodUrls.map(function(url){return "<Play>" + url + "</Play>";}).join("\n") + 
+                    '</Response>');
+                });
+
+        });
+    }); 
+    
 }).listen(1337);
 console.log('Server running at http://127.0.0.1:1337/');
