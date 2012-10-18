5d9c8aa1850a302856f2c1afd8840e3391f64d00
Jacob Beard <jbeard4@cs.mcgill.ca>
2012-10-17 20:45:28 -0400

    Added functionality to retrieve a list of sessions.

--- serve.js
+++ serve.js
@@ -15,6 +15,20 @@ scion.ext.actionCodeGeneratorModule.gen.actionTags[""].Response = function(actio
     return s;
 }; 
 
+function dumpSessionsHTML(res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(
+        '<html><head></head><body><ul><h1>Active Sessions:</h1>' + 
+            Object.keys(sessions).map(function(sid){
+                return '<li><a href="/sessions/' + sid + '">/sessions/' + sid + '</a></li>';
+            }).join('\n') + 
+        '</ul></body></html>');
+}
+
+function dumpSessionHTML(res,session){
+    //pull it out
+}
+
 scion.pathToModel('./archive.xml',function(err,model){
     if(err) throw err;
 
@@ -22,39 +36,57 @@ scion.pathToModel('./archive.xml',function(err,model){
 
         //do everything as GET
         var url = urlModule.parse(req.url,true);
-        var sid = url.query.CallSid;
 
-        //pull out the session or create if not created
-        var session = sessions[sid];
-        if(!session){
-            console.log("creating new SCXML session");
-            session = sessions[sid] = new scion.SCXML(model);
-            session.start();
-            session.gen("init",{
-                http:http,
-                url:urlModule,
-                archive:require('./archive-lib'),
-                async:require('async')
-            });
-        }else{
-            console.log("using existing session");
-        }
+        var m;
 
-        //transform this into a statecharts event and pass into the state machine
-        var event = {
-            name : url.pathname,
-            data : {
-                request : req,
-                response : res,
-                params : url.query
+        if(url.pathname === "/sessions" && req.method === "GET"){
+            dumpSessionsHTML(res);
+        }else if(m = url.pathname.match(/^\/sessions\/(.*)$/)){
+            dumpSessionHTML(res,m[1]);
+        }else if(url.query.CallSid){
+
+            var sid = url.query.CallSid;
+
+            if(url.query.CallStatus === 'completed'){
+                delete sessions[sid];   //TODO: send this into the state maichne as an event as well, such that it transitions to final state and cleans itself up.
+            }else{
+
+                //pull out the session or create if not created
+                var session = sessions[sid];
+                if(!session){
+                    console.log("creating new SCXML session");
+                    session = sessions[sid] = new scion.SCXML(model);
+                    session.start();
+                    session.gen("init",{
+                        http:http,
+                        url:urlModule,
+                        archive:require('./archive-lib'),
+                        async:require('async')
+                    });
+                }else{
+                    console.log("using existing session");
+                }
+
+                //transform this into a statecharts event and pass into the state machine
+                var event = {
+                    name : url.pathname,
+                    data : {
+                        request : req,
+                        response : res,
+                        params : url.query
+                    }
+                };
+
+                console.log("sending event",event);
+
+                var conf = session.gen(event);
+
+                console.log("new conf",conf);
             }
-        };
-
-        console.log("sending event",event);
-
-        var conf = session.gen(event);
-
-        console.log("new conf",conf);
+        }else{
+            res.writeHead(400, {'Content-Type': 'text/plain'});
+            res.end('Server did not understand request.\n' + JSON.stringify(url,4,4));
+        }
         
     }).listen(1337);
     console.log('Server running at http://127.0.0.1:1337/');
